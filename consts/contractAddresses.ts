06103b328ef15945ae59e7590e0b9afa26000489
seunghyonpark <creath.park@gmail.com>
2023-05-28 22:43:07 +0900

    first commit

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x5574f457D72Ee0fCe90C88F9536e976545427e69";
+export const tokenContractAddress =
+  "0x1660d6332118a4754AE0061B42015967fa4444fd";
+export const stakingContractAddress =
+  "0xf454D80542B810AF9e5f5BedcE52c3FD41B3285b";
